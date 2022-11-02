5fc56ce069b0bb14556e0c2af5c1e69e3ca92a71
Jesserc <raymondjesse713@gmail.com>
2022-11-02 15:08:24 +0100

    deploy website

--- src/index.js
+++ src/index.js
@@ -24,8 +24,8 @@ ReactDOM.render(
         chainId: 44787,
       }}
       dapp={{
-        name: "Celo React Boilerplate",
-        description: "A React Boilerplate for Celo Dapps",
+        name: "Celo Anime Collection",
+        description: "Anime Collection built on Celo",
       }}
     >
       <App />
